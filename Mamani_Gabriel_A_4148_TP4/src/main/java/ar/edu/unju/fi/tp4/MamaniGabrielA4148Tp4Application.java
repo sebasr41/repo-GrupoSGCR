8e63eddd7d0a471983e0e37037306fbb855ae537
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-05 21:09:42 -0300

    subiendo el proyecto al repositorio

---
+++ Mamani_Gabriel_A_4148_TP4/src/main/java/ar/edu/unju/fi/tp4/MamaniGabrielA4148Tp4Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamaniGabrielA4148Tp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamaniGabrielA4148Tp4Application.class, args);
+	}
+
+}
