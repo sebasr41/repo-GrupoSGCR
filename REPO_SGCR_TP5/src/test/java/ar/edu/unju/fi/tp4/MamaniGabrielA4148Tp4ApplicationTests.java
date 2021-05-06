7fb90207e39ddf1e8d319778070c69bb67accc62
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-05 21:45:21 -0300

    task : nuestro proyecto

---
+++ REPO_SGCR_TP5/src/test/java/ar/edu/unju/fi/tp4/MamaniGabrielA4148Tp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamaniGabrielA4148Tp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
